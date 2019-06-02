ea963ca67fb5a00d87d9a19ec446fb8f7527b646
Sandra <sandra.giermanska@gmail.com>
2019-06-02 14:21:54 +0200

    update view window

--- qt/view.py
+++ qt/view.py
@@ -8,7 +8,7 @@ class App(QWidget):
     def __init__(self, folderNumber, imagesAmount):
         super().__init__()
         self.title = 'Simulation 0/' + str(imagesAmount)
-        self.imageNumber = 0
+        self.imageNumber = 0.0
         self.folderNumber = folderNumber
         self.imagesAmount = imagesAmount
         self.initUI()
@@ -19,13 +19,13 @@ class App(QWidget):
             if self.imageNumber < self.imagesAmount:
                 if self.showImage(self.folderNumber, self.imageNumber + 1):
                     self.imageNumber = self.imageNumber + 1
-                    self.setWindowTitle('Simulation ' + str(self.imageNumber) + '/' + str(self.imagesAmount))
+                    self.setWindowTitle('Simulation ' + str(int(self.imageNumber)) + '/' + str(self.imagesAmount))
                 
         if key == Qt.Key_Left:
             if self.imageNumber > 0:
                 if self.showImage(self.folderNumber, self.imageNumber - 1):
                     self.imageNumber = self.imageNumber - 1
-                    self.setWindowTitle('Simulation ' + str(self.imageNumber) + '/' + str(self.imagesAmount))
+                    self.setWindowTitle('Simulation ' + str(int(self.imageNumber)) + '/' + str(self.imagesAmount))
                 
         if key == Qt.Key_Escape:
                 self.close()
@@ -38,19 +38,22 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setWindowIcon(QIcon('test.png'))
         self.setGeometry(400, 100, 200, 200)
-        self.showImage(self.folderNumber, 0)
+        self.showImage(self.folderNumber, 0.0)
         self.show()
 
     def showImage(self, folderNumber, imageNumber):
+        number = str(imageNumber)
+        if number == "0.0":
+            file = "0.png"
+        else:
+            file = number + ".png"
         if platform.system() == 'Linux':
             directory = "./tmp/img" + str(folderNumber)
-            pixmap = QPixmap(directory + "/" + str(imageNumber) + ".png")
+            pixmap = QPixmap(directory + "/" + file)
         else:
-            directory = "tmp/img" + str(folderNumber)
-            imagelist = ["0.png","1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.png","8.0.png","9.0.png",
-                         "10.0.png","11.0.png","12.0.png","13.0.png","14.0.png","15.0.png","16.0.png","17.0.png","18.0.png",
-                         "19.0.png","20.0.png"]
-            pixmap = QPixmap(directory + '\\' + imagelist[imageNumber])
+            directory = "tmp\\img" + str(folderNumber)
+            pixmap = QPixmap(directory + "\\" + file)
+
         if not pixmap.isNull():
             pixmapResized = pixmap.scaled(800, 600, Qt.KeepAspectRatio)
             self.label.setPixmap(pixmapResized)
